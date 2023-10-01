38130cdccbbd06d887d99a9073f5e91980772dc0
mhalaider <mhalaider@gmail.com>
2023-10-01 16:08:47 +0300

    Clear pasted shit

--- ExternalCS2/Source/Helpers/Cacher/CCachedList.cpp
+++ ExternalCS2/Source/Helpers/Cacher/CCachedList.cpp
@@ -6,66 +6,15 @@
 
 #include <vector>
 
-constexpr auto MAX_ENTITIES_IN_LIST = 512;
-constexpr auto MAX_ENTITY_LISTS = 64; // 0x3F
-constexpr auto MAX_TOTAL_ENTITIES = MAX_ENTITIES_IN_LIST * MAX_ENTITY_LISTS; // 0x8000
-
-class CEntityIdentity {
-public:
-    void* m_pEntity;
-    void* dunno;
-    int                     entHandle; // LOWORD(handle) & 0x7FFF = entID
-    int                     m_nameStringableIndex; // always seems to be -1
-    const char* internalName; // these two strings are optional!
-    const char* m_designerName; // ex: item_tpscroll
-private:
-    std::uint8_t __pad0028[0x8]; // 0x28
-public:
-    std::uint32_t m_flags; // 0x30
-private:
-    std::uint8_t __pad0034[0x4]; // 0x34
-public:
-    int           m_worldGroupId; // 0x38
-    std::uint32_t m_fDataObjectTypes; // 0x3c
-    short         m_PathIndex; // 0x40
-private:
-    std::uint8_t __pad0042[0x16]; // 0x42
-public:
-    CEntityIdentity* m_pPrev; // 0x58
-    CEntityIdentity* m_pNext; // 0x60
-    CEntityIdentity* m_pPrevByClass; // 0x68
-    CEntityIdentity* m_pNextByClass; // 0x70
-}; // sizeof == 0x78
-
-class CEntityIdentities {
-public:
-    CEntityIdentity m_pIdentities[MAX_ENTITY_LISTS][MAX_ENTITIES_IN_LIST];
-};
-
-inline auto g_pLocalIdentityChunks = new CEntityIdentities();
-
 class CGameEntitySystem {
-    char         pad[16];
-    CEntityIdentities* m_pIdentityChunks;
 public:
-    void Update()
-    {
-        return;
-        static std::uintptr_t ppAddr = g_pMemoryManager->GetClientInfo().dwBaseAddress + Offsets::EntityList;
-        const auto            pAddr = g_pMemoryManager->RPM< std::uintptr_t >(ppAddr);
-
-        g_pMemoryManager->RPM< CGameEntitySystem >(pAddr, this);
-        g_pMemoryManager->RPM< CEntityIdentities >(reinterpret_cast<std::uintptr_t>(this->m_pIdentityChunks), g_pLocalIdentityChunks);
-    }
 
     template < class tClass = std::uintptr_t >
     tClass GetPlayerController(const std::int32_t index) {
         static std::uintptr_t ppAddr = g_pMemoryManager->GetClientInfo().dwBaseAddress + Offsets::EntityList;
+
         std::uintptr_t entityList = g_pMemoryManager->RPM(ppAddr);
-        // ENTITY_SPACING = 0x78
-        // i goes from 1 to 64 in a loop
         std::uintptr_t listEntry = g_pMemoryManager->RPM(entityList + (8 * (index & 0x7FFF) >> 9) + 16);
-        // CCSPlayerController 
         std::uintptr_t controllerBase = g_pMemoryManager->RPM(listEntry + 0x78 * (index & 0x1FF));
 
         return static_cast<tClass>(controllerBase);
@@ -74,8 +23,8 @@ public:
     template < class tClass = std::uintptr_t>
     tClass GetEntityFromHandle(const std::uint32_t uiHandle) {
         static std::uintptr_t ppAddr = g_pMemoryManager->GetClientInfo().dwBaseAddress + Offsets::EntityList;
-        std::uintptr_t entityList = g_pMemoryManager->RPM(ppAddr);
 
+        std::uintptr_t entityList = g_pMemoryManager->RPM(ppAddr);
         std::uintptr_t listEntry2 = g_pMemoryManager->RPM(entityList + 0x8 * ((uiHandle & 0x7FFF) >> 9) + 16);
 
         return static_cast<tClass>(g_pMemoryManager->RPM(listEntry2 + 0x78 * (uiHandle & 0x1FF)));
@@ -112,8 +61,6 @@ void CCachedEntityList::Instance() {
     const auto iLocalTeam = g_pMemoryManager->RPM<std::int32_t>(pLocalPlayerPawn + Offsets::iTeamNum);
 
     const auto pViewMatrix = g_pMemoryManager->RPM< VMatrix >(clientBase + Offsets::ViewMatrix);
-
-    pEntitySystem->Update();
 
     for (int iEntityIdx = 1; iEntityIdx < 64; iEntityIdx++) {
         auto pPlayer = GeniusPtr(pEntitySystem->GetPlayerController(iEntityIdx), 0x848);
